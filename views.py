83d9aa812d66ec7758a423e0852511999181858f
GUNNER <gunner@ubuntu.ubuntu-domain>
2013-10-01 12:04:28 +0800

    modified:   templates/base.html 	modified:   templates/list.html 	modified:   views.py

--- views.py
+++ views.py
@@ -3,6 +3,7 @@ from flask import Blueprint, request, redirect, render_template, url_for
 from flask.views import MethodView
 from models import * 
 import re
+from update import find_show
 
 shows = Blueprint('posts', __name__, template_folder='templates')
 
@@ -11,7 +12,12 @@ class ListView(MethodView):
     def get(self):
         shows = Show.objects()
         return render_template('list.html', shows=shows)
-
+    def post(self):
+	show_name = request.form['show_name']
+	print show_name
+	if show_name != '':	
+	    find_show(show_name)
+        return redirect('/')
 	
 
 shows.add_url_rule('/', view_func=ListView.as_view('list'))
