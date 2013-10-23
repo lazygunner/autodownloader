ade9ba9f0a8e657e96d5a1ff235eb426c4551464
lazygunner <gymgunner@gmail.com>
2013-10-23 10:01:51 +0800

    finish the follow function

--- details.py
+++ details.py
@@ -1,6 +1,7 @@
 from models import *
 from flask.views import MethodView
 from flask import Blueprint, render_template
+from flask.ext.login import current_user
 
 details = Blueprint('details', __name__, template_folder='templates')
 
@@ -8,6 +9,16 @@ class DetailView(MethodView):
     
     def get(self, show_id, format='HR-HDTV'):
         show = Show.objects.get(show_id=show_id)
+        follow = 'anonymous'
+        if current_user.is_anonymous():
+            follow = 'anonymous'
+        else:
+            followings = len(Following.objects(user_id=current_user.id, show_id=show_id))
+            if followings > 0:
+                follow = 'unfollow'
+            else:
+                follow = 'follow'
+                
         episodes_array = []
         for i in range(1,100):
             episodes = Episode.objects(show_id = show_id, format=format, season=i).order_by('+index')
@@ -15,9 +26,9 @@ class DetailView(MethodView):
                 episodes_array.append(episodes)
             else:
                 break
-        return render_template('detail.html', show=show, episodes_array = episodes_array)
+        return render_template('detail.html', show=show, episodes_array = episodes_array, follow=follow)
         
 
 
 
-details.add_url_rule('/details/<show_id>/', view_func=DetailView.as_view('detail'))
+details.add_url_rule('/details/<show_id>/', view_func=DetailView.as_view('details'))
